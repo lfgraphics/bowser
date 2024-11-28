cbd86cb002181413eb6838d0da2e0b2aead3f5f5
Taha Kazmi <96362790+lfgraphics@users.noreply.github.com>
2024-11-28 18:49:21 +0530

    added updates routes and fixed some dependencies issues

--- backend/src/models/updates.js
+++ backend/src/models/updates.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const moment = require("moment-timezone");
 const { UsersAndRolesDatabaseConnection } = require('../../config/database');
 
 const updatesSchema = new mongoose.Schema({
     buildVersion: Number,
     url: String,
-    pushDate: Date
+    pushDate: { type: Date, default: () => moment().tz("Asia/Kolkata").toDate() }
 });
 
 module.exports = UsersAndRolesDatabaseConnection.model('Update', updatesSchema, 'updates');
--- backend/src/routes/updates.js
+++ backend/src/routes/updates.js
@@ -5,11 +5,49 @@ const Update = require('../models/updates');
 // Get all users with roles populated
 router.get('/', async (req, res) => {
     try {
-        const updates = await Update.find().sort({ pushDate: -1 }).exec();
+        const updates = await Update.find().sort({ buildVersion: -1 }).exec();
         res.status(200).json(updates);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch updates', details: error });
+    }
+});
+router.get('/:id', async (req, res) => {
+    try {
+        const update = await Update.findOne(req.params.id);
+        res.status(200).json(update);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch update', details: error });
+    }
+});
+router.post('/', async (req, res) => {
+    try {
+        let newUpdate = new Update(req.body)
+        const saveOptions = {
+            writeConcern: {
+                w: 'majority',
+                wtimeout: 30000
+            }
+        };
+
+        const savePromise = newUpdate.save(saveOptions);
+        const timeoutPromise = new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('Save operation timed out')), 35000)
+        );
+
+        await Promise.race([savePromise, timeoutPromise]);
+
+        res.status(200).json({ message: 'Data Submitted successfully' });
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch users', details: error });
     }
 });
-
+router.delete('/:id', async (req, res) => {
+    let { updateId } = req.params
+    try {
+        await Update.deleteOne(updateId);
+        res.status(200).json({ title: 'Operation Successful', message:'Deleted updated successfully' })
+    } catch (err) {
+        res.status(400).json({ title: err.title, message: err.message })
+    }
+})
 module.exports = router;
