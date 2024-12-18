9c39e2f9cc326178555cf26ecb42bc2e59500817
Taha Kazmi <96362790+lfgraphics@users.noreply.github.com>
2024-12-18 11:24:30 +0530

    Backend url update

--- bowser-admin/lib/auth.ts
+++ bowser-admin/lib/auth.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { User, LoginResponse, SignupResponse } from '../types/auth';
 import { registerPushSubscription, unregisterPushSubscription } from '@/utils/pushNotifications';
 
-export const API_URL = 'http://192.168.137.1:5000'; //https://bowser-backend-2cdr.onrender.com //http://192.168.137.1:5000
+export const API_URL = 'https://bowser-backend-2cdr.onrender.com'; //https://bowser-backend-2cdr.onrender.com //http://192.168.137.1:5000
 
 export async function signup(userData: { userId: string; password: string; name: string; phoneNumber: string }): Promise<SignupResponse> {
     try {
