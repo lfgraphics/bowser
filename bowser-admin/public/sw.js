3db5c2421679896fb1b965308688ee3e8acd7d47
Taha Kazmi <96362790+lfgraphics@users.noreply.github.com>
2024-12-18 13:40:10 +0530

    service worker registration fix check in production

--- bowser-admin/public/sw.js
+++ bowser-admin/public/sw.js
@@ -1,37 +1,3 @@
-const CACHE_NAME = 'my-app-cache-v1';
-const urlsToCache = [
-  '/',
-  '/favicon.ico',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png',
-  '/styles/global.css',
-];
-
-// During installation, cache necessary assets
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// During activation, clean up old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
 // Intercept fetch requests and serve from cache if available
 self.addEventListener('fetch', event => {
   event.respondWith(
@@ -64,14 +30,3 @@ self.addEventListener('notificationclick', function (event) {
   event.notification.close()
   event.waitUntil(clients.openWindow('https://itpl-bowser-admin.vercel.app'))
 })
-
-self.addEventListener('fetch', event => {
-  // Ensure only requests within scope are handled
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  }
-});
