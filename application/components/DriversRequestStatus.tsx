9fdaba88a53897bb1f5c74c01fd5678de1496228
Taha Kazmi <lfgsbusinessmail@gmail.com>
2025-05-17 11:28:30 +0530

    new feature intigration
    added fuel providers logo and linked with drivers fuel request status for eas of illitrate drivers

--- application/components/DriversRequestStatus.tsx
+++ application/components/DriversRequestStatus.tsx
@@ -1,11 +1,24 @@
 import { DriverFuelRequest } from '@/src/types';
-import { baseUrl } from '@/src/utils/helpers';
+import { baseUrl, blurhash } from '@/src/utils/helpers';
 import React from 'react'
 import { ThemedView } from './ThemedView';
 import { ThemedText } from './ThemedText';
-import { ActivityIndicator, Button, Linking, StyleSheet, TouchableOpacity, View } from 'react-native';
+// import { Image } from 'expo-image';
+import { ActivityIndicator, Button, Linking, StyleSheet, TouchableOpacity, View, Image } from 'react-native';
 import { Link } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+const HPCL = require('@/assets/images/fuel-pumps/HPCL.jpg');
+const IOCL = require('@/assets/images/fuel-pumps/IOCL.jpg');
+const BPCL = require('@/assets/images/fuel-pumps/BPCL.jpg');
+const Reliance = require('@/assets/images/fuel-pumps/Reliance.jpg');
+type FuelProvider = 'HPCL' | 'IOCL' | 'BPCL' | 'Reliance';
+
+const imageMap: Record<FuelProvider, any> = {
+    HPCL,
+    IOCL,
+    BPCL,
+    Reliance,
+};
 
 interface Props {
     requestId: string;
@@ -36,57 +49,81 @@ const DriversRequestStatus: React.FC<Props> = ({ requestId }) => {
         console.log("Request ID: ", requestId);
     }, []);
 
+    console.log('Reliance Image: ', HPCL);
+
+    console.log("Image Data: ", imageMap[(orderData?.allocation?.fuelProvider as FuelProvider) || 'Reliance']);
+
     return (
-        <>{loading && <View style={styles.loaderBg}>
-            <ActivityIndicator color="#0a7ea4" />
-        </View>}
-            <ThemedView style={{ padding: 12, borderRadius: 6, gap: 6, paddingTop: 16 }} >
-                <ThemedText style={{ textAlign: "center", fontSize: 20, fontWeight: "bold" }}>आपके डीज़ल अनुरोध की जानकारी</ThemedText>
-                {orderData && (!orderData.allocation && !orderData.message) && <ThemedText style={{ textAlign: "center" }}>आपका डीज़ल अनुरोध अभी पूरा नहीं हुआ है, कृपया थोड़ी देर बाद दोबारा चेक करें।</ThemedText>}
-                {orderData && !orderData.allocation && orderData.message && <ThemedText style={{ textAlign: "center" }}>{orderData.message}</ThemedText>}
-                {orderData && orderData.allocation?.allocationType == "bowser" && (
-                    <ThemedView>
-                        {orderData.allocation?.bowser.driver.location &&
-                            <Link style={styles.button} href={`https://www.google.com/maps/dir/?api=1&destination=${orderData.allocation?.bowser.driver.location?.replace(' ', '')}` as any}>
-                                बाउज़र की लोकेशन देखें
-                            </Link>
-                        }
-                        {orderData.allocation &&
-                            <>
-                                <ThemedText>
-                                    बाउज़र: गाड़ी नं0 {orderData.allocation?.bowser.regNo}, ड्राईवर {orderData.allocation?.bowser.driver.name}
-                                </ThemedText>
-                                <ThemedText>
-                                    तेल मिलेगा: {orderData.allocation?.quantityType === "Full" ? "फुल" : "पार्ट"}, {orderData.allocation?.fuelQuantity > 0 ? orderData.allocation?.fuelQuantity + "लीटर" : ""}
-                                </ThemedText>
-                                <TouchableOpacity style={styles.button} onPress={() => Linking.openURL(`tel:${orderData.allocation?.bowser.driver.phoneNo}`)}>
-                                    <ThemedText>बाउज़र ड्राईवर को काल करें</ThemedText><Ionicons name="call" size={18} color="white" />
-                                </TouchableOpacity>
-                            </>
-                        }
-                    </ThemedView>
-                )}
-                {orderData && orderData.allocation?.allocationType == "external" && (
-                    <ThemedView>
-                        {orderData.allocation &&
-                            <>
-                                <ThemedText>
-                                    डीज़ल ले लीजिये: {orderData.allocation?.quantityType === "Full" ? "फुल" : "पार्ट"}, {orderData.allocation?.fuelQuantity > 0 ? orderData.allocation?.fuelQuantity + "लीटर" : ""}
-                                </ThemedText>
-                                <ThemedText>
-                                    {orderData.allocation?.fuelProvider} के {orderData.allocation?.pumpAllocationType === "Any" ? "किसी भी पेट्रोल पंप से" : orderData.allocation?.pumpLocation + "पेट्रोल पंप से"}
-                                </ThemedText>
-                            </>
-                        }
-                    </ThemedView>
-                )}
-                {orderData && !orderData.message && <Button
-                    onPress={fetchData}
-                    title="दोबारा चेक करें"
-                    color="#0a7ea4"
-                    accessibilityLabel="Refresh your fuel request status"
-                />}
-            </ThemedView >
+        <>
+            {loading &&
+                <View style={styles.loaderBg}>
+                    <ActivityIndicator color="#0a7ea4" />
+                </View>
+            }
+            {orderData &&
+                <ThemedView style={{ padding: 12, borderRadius: 6, gap: 6, paddingTop: 16 }} >
+                    <ThemedText style={{ textAlign: "center", fontSize: 20, fontWeight: "bold" }}>आपके डीज़ल अनुरोध की जानकारी</ThemedText>
+                    {orderData && (!orderData.allocation && !orderData.message) && <ThemedText style={{ textAlign: "center" }}>आपका डीज़ल अनुरोध अभी पूरा नहीं हुआ है, कृपया थोड़ी देर बाद दोबारा चेक करें।</ThemedText>}
+                    {orderData && !orderData.allocation && orderData.message && <ThemedText style={{ textAlign: "center" }}>{orderData.message}</ThemedText>}
+                    {orderData && orderData.allocation?.allocationType == "bowser" && (
+                        <ThemedView>
+                            {orderData.allocation?.bowser.driver.location &&
+                                <Link style={styles.button} href={`https://www.google.com/maps/dir/?api=1&destination=${orderData.allocation?.bowser.driver.location?.replace(' ', '')}` as any}>
+                                    बाउज़र की लोकेशन देखें
+                                </Link>
+                            }
+                            {orderData.allocation &&
+                                <>
+                                    <ThemedText>
+                                        बाउज़र: गाड़ी नं0 {orderData.allocation?.bowser.regNo}, ड्राईवर {orderData.allocation?.bowser.driver.name}
+                                    </ThemedText>
+                                    <ThemedText>
+                                        तेल मिलेगा: {orderData.allocation?.quantityType === "Full" ? "फुल" : "पार्ट"}, {orderData.allocation?.fuelQuantity > 0 ? orderData.allocation?.fuelQuantity + "लीटर" : ""}
+                                    </ThemedText>
+                                    <TouchableOpacity style={styles.button} onPress={() => Linking.openURL(`tel:${orderData.allocation?.bowser.driver.phoneNo}`)}>
+                                        <ThemedText>बाउज़र ड्राईवर को काल करें</ThemedText><Ionicons name="call" size={18} color="white" />
+                                    </TouchableOpacity>
+                                </>
+                            }
+                        </ThemedView>
+                    )}
+                    {orderData && orderData.allocation?.allocationType == "external" && (
+                        <ThemedView>
+                            {orderData.allocation &&
+                                <>
+                                    <ThemedText>
+                                        डीज़ल ले लीजिये: {orderData.allocation?.quantityType === "Full" ? "फुल" : "पार्ट"}, {orderData.allocation?.fuelQuantity > 0 ? orderData.allocation?.fuelQuantity + "लीटर" : ""}
+                                    </ThemedText>
+                                    <ThemedText>
+                                        {orderData.allocation?.fuelProvider} के {orderData.allocation?.pumpAllocationType === "Any" ? "किसी भी पेट्रोल पंप से" : orderData.allocation?.pumpLocation + "पेट्रोल पंप से"}
+                                    </ThemedText>
+                                    <Image
+                                        source={imageMap[(orderData.allocation?.fuelProvider as FuelProvider) || 'Reliance']}
+                                        style={styles.image}
+                                    />
+                                </>
+                            }
+                        </ThemedView>
+                    )}
+                    {orderData && orderData.allocation?.allocationType == "internal" && (
+                        <ThemedView>
+                            {orderData.allocation &&
+                                <>
+                                    <ThemedText>
+                                        {orderData.allocation?.bowser.driver.name} से {orderData.allocation?.quantityType === "Full" ? "फुल" : ""}, {orderData.allocation?.fuelQuantity > 0 ? orderData.allocation?.fuelQuantity + " लीटर" : ""} डीज़ल ले लीजिये
+                                    </ThemedText>
+                                </>
+                            }
+                        </ThemedView>
+                    )}
+                    {orderData && !orderData.message && <Button
+                        onPress={fetchData}
+                        title="दोबारा चेक करें"
+                        color="#0a7ea4"
+                        accessibilityLabel="Refresh your fuel request status"
+                    />}
+                </ThemedView >
+            }
         </>
     )
 }
@@ -102,6 +139,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
         zIndex: 1000,
+    },
+    image: {
+        marginVertical: 4,
+        borderRadius: 5,
+        marginHorizontal: 'auto',
+        maxHeight: 250,
+        width: 250,
     },
     button: {
         flexDirection: 'row',
--- application/src/utils/helpers.ts
+++ application/src/utils/helpers.ts
@@ -95,3 +95,5 @@ export const stopSharingLocation = (orderId: string) => {
         socket = null;
     }
 };
+
+export const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
