b0b8c8bf6545f06a47484d83d4e3a51748b25ab4
Taha Kazmi <lfgsbusinessmail@gmail.com>
2025-08-29 17:47:09 +0530

    UI UX enhancement

--- bowser-admin/app/trans-app/manage-profile/page.tsx
+++ bowser-admin/app/trans-app/manage-profile/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { isAuthenticated } from '@/lib/auth';
-import { TransAppUser, User } from '@/types';
+import { TransAppUser } from '@/types';
 import Loading from '@/app/loading';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -52,22 +52,6 @@ const Profile = () => {
         setEditingField(null);
     };
 
-    const handlePasswordReset = async () => {
-        const userId = user?.userId;
-        setLoading(true);
-        setError('');
-
-        try {
-            const response = await axios.post(`${BASE_URL}/auth/generate-reset-url`, { userId });
-            const resetUrl = response.data.resetUrl;
-            window.location.href = resetUrl;
-        } catch (err) {
-            setError((err as any).response?.data?.message || 'An error occurred while generating the reset URL.');
-        } finally {
-            setLoading(false);
-        }
-    };
-
     const isUpdated = name !== initialName || phoneNumber !== initialPhoneNumber || password.length > 0;
 
     const handleUpdateUser = async () => {
@@ -111,64 +95,66 @@ const Profile = () => {
     };
 
     return (
-        <div className="flex justify-center items-center bg-background min-h-screen">
+        <>
             {loading && <Loading />}
-            <Card className="w-[350px]">
-                <CardHeader>
-                    <CardTitle>Profile</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="flex flex-col space-y-4">
-                        <div className="flex justify-between gap-3">
-                            <Label className='flex-[0.9]'>User Id</Label>
-                            <span>{user?.userId}</span>
-                        </div>
-                        <div className="flex justify-between gap-3">
-                            <Label className='flex-[0.9]'>Role/s</Label>
-                            <span>{user?.Division}</span>
-                        </div>
-                        <Separator />
+            <div className="flex justify-center items-center bg-background min-h-screen">
+                <Card className="w-[350px]">
+                    <CardHeader>
+                        <CardTitle>Profile</CardTitle>
+                    </CardHeader>
+                    <CardContent>
                         <div className="flex flex-col space-y-4">
-                            <div className="flex justify-between items-center gap-3">
-                                <Label className='flex-[0.9]'>Name</Label>
-                                {editingField === 'name' ? (
-                                    <Input value={name} onChange={(e) => setName(e.target.value)} className="flex-[1]" />
-                                ) : (
-                                    <span className="flex-[1">{name}</span>
-                                )}
-                                <span onClick={() => editingField === 'name' ? handleSave() : handleEdit('name')}>
-                                    {editingField === 'name' ? <Check size={18} color='white' /> : <Edit2 size={18} color='gray' />}
-                                </span>
+                            <div className="flex justify-between gap-3">
+                                <Label className='flex-[0.9]'>User Id</Label>
+                                <span>{user?.userId}</span>
                             </div>
-                            <div className="flex justify-between items-center gap-3">
-                                <Label className='flex-[0.9]'>Phone Number</Label>
-                                {editingField === 'phoneNumber' ? (
-                                    <Input type='number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="flex-[1]" />
-                                ) : (
-                                    <span className="flex-[1">{phoneNumber}</span>
-                                )}
-                                <span onClick={() => editingField === 'phoneNumber' ? handleSave() : handleEdit('phoneNumber')}>
-                                    {editingField === 'phoneNumber' ? <Check size={18} color='white' /> : <Edit2 size={18} color='gray' />}
-                                </span>
+                            <div className="flex justify-between gap-3">
+                                <Label className='flex-[0.9]'>Role/s</Label>
+                                <span>{user?.Division}</span>
                             </div>
-                            <div className="flex justify-between items-center gap-3">
-                                <Label className='flex-[0.9]'>Password</Label>
-                                {editingField === 'password' ? (
-                                    <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} className="flex-[1]" />
-                                ) : (
-                                    '*'.repeat(password.length)
-                                )}
-                                <span onClick={() => editingField === 'password' ? handleSave() : handleEdit('password')}>
-                                    {editingField === 'password' ? <Check size={18} color='white' /> : <Edit2 size={18} color='gray' />}
-                                </span>
+                            <Separator />
+                            <div className="flex flex-col space-y-4">
+                                <div className="flex justify-between items-center gap-3">
+                                    <Label className='flex-[0.9]'>Name</Label>
+                                    {editingField === 'name' ? (
+                                        <Input value={name} onChange={(e) => setName(e.target.value)} className="flex-[1]" />
+                                    ) : (
+                                        <span className="flex-[1">{name}</span>
+                                    )}
+                                    <span onClick={() => editingField === 'name' ? handleSave() : handleEdit('name')}>
+                                        {editingField === 'name' ? <Check size={18} color='white' /> : <Edit2 size={18} color='gray' />}
+                                    </span>
+                                </div>
+                                <div className="flex justify-between items-center gap-3">
+                                    <Label className='flex-[0.9]'>Phone Number</Label>
+                                    {editingField === 'phoneNumber' ? (
+                                        <Input type='number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="flex-[1]" />
+                                    ) : (
+                                        <span className="flex-[1">{phoneNumber}</span>
+                                    )}
+                                    <span onClick={() => editingField === 'phoneNumber' ? handleSave() : handleEdit('phoneNumber')}>
+                                        {editingField === 'phoneNumber' ? <Check size={18} color='white' /> : <Edit2 size={18} color='gray' />}
+                                    </span>
+                                </div>
+                                <div className="flex justify-between items-center gap-3">
+                                    <Label className='flex-[0.9]'>Password</Label>
+                                    {editingField === 'password' ? (
+                                        <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} className="flex-[1]" />
+                                    ) : (
+                                        '*'.repeat(password.length)
+                                    )}
+                                    <span onClick={() => editingField === 'password' ? handleSave() : handleEdit('password')}>
+                                        {editingField === 'password' ? <Check size={18} color='white' /> : <Edit2 size={18} color='gray' />}
+                                    </span>
+                                </div>
                             </div>
+                            {isUpdated && <Button onClick={handleUpdateUser}>Save Changes</Button>}
+                            {error && <p className="text-red-500">{error}</p>}
                         </div>
-                        {isUpdated && <Button onClick={handleUpdateUser}>Save Changes</Button>}
-                        {error && <p className="text-red-500">{error}</p>}
-                    </div>
-                </CardContent>
-            </Card>
-        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
     );
 };
 
